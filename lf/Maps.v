82bfa1636165cd34eadb99f55d9312e9f0fefd77
EncodeTalker <2100011032@stu.pku.edu.cn>
2023-04-07 23:11:00 +0800

    finished Maps.v

--- lf/Maps.v
+++ lf/Maps.v
@@ -187,7 +187,7 @@ Proof. reflexivity. Qed.
 Lemma t_apply_empty : forall (A : Type) (x : string) (v : A),
   (_ !-> v) x = v.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros. reflexivity. Qed.
 (** [] *)
 
 (** **** Exercise: 2 stars, standard, optional (t_update_eq)
@@ -199,7 +199,8 @@ Proof.
 Lemma t_update_eq : forall (A : Type) (m : total_map A) x v,
   (x !-> v ; m) x = v.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros. unfold t_update.
+  rewrite String.eqb_refl. reflexivity. Qed.
 (** [] *)
 
 (** **** Exercise: 2 stars, standard, optional (t_update_neq)
@@ -212,7 +213,11 @@ Theorem t_update_neq : forall (A : Type) (m : total_map A) x1 x2 v,
   x1 <> x2 ->
   (x1 !-> v ; m) x2 = m x2.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros. unfold t_update.
+  destruct (String.eqb x1 x2) eqn:H1.
+  - apply String.eqb_eq in H1. contradiction.
+  - reflexivity. 
+Qed.
 (** [] *)
 
 (** **** Exercise: 2 stars, standard, optional (t_update_shadow)
@@ -226,7 +231,11 @@ Proof.
 Lemma t_update_shadow : forall (A : Type) (m : total_map A) x v1 v2,
   (x !-> v2 ; x !-> v1 ; m) = (x !-> v2 ; m).
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros. apply functional_extensionality. intros. 
+  unfold t_update. destruct (eqb_spec x x0).
+  - reflexivity.
+  - reflexivity.
+Qed.
 (** [] *)
 
 (** **** Exercise: 2 stars, standard (t_update_same)
@@ -243,7 +252,11 @@ Proof.
 Theorem t_update_same : forall (A : Type) (m : total_map A) x,
   (x !-> m x ; m) = m.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros. apply functional_extensionality. intros.
+  unfold t_update. destruct (eqb_spec x x0).
+  - rewrite e. reflexivity.
+  - reflexivity.
+Qed.
 (** [] *)
 
 (** **** Exercise: 3 stars, standard, especially useful (t_update_permute)
@@ -259,7 +272,19 @@ Theorem t_update_permute : forall (A : Type) (m : total_map A)
   =
   (x2 !-> v2 ; x1 !-> v1 ; m).
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros.
+  apply functional_extensionality. intros.
+  unfold t_update.
+  destruct (eqb_spec x1 x) as [H1 | H1].
+  - destruct (eqb_spec x2 x) as [H2 | H2].
+	+ exfalso. apply H.
+	  rewrite H1. rewrite H2. reflexivity.
+	+ reflexivity.
+  - destruct (eqb_spec x2 x) as [H2 | H2].
+    + reflexivity.
+	+ reflexivity.
+Qed.
+
 (** [] *)
 
 (* ################################################################# *)
